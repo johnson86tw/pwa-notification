520584125914933919bdeb362511ae5ef20f7f2a
Johnson Chen <chnejohnson@gmail.com>
2024-07-01 17:36:06 +0800

    add sw.ts and requestNotificationPermission

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const requestResult = ref('')
+async function requestNotificationPermission() {
+	if (!('Notification' in window)) {
+		requestResult.value = 'Browser does not support notifications'
+	}
+
+	const permission = await Notification.requestPermission()
+
+	if (permission !== 'granted') {
+		requestResult.value = 'Permission not granted'
+		throw new Error('Permission not granted')
+	} else {
+		requestResult.value = 'Permission granted'
+		new Notification('Permission granted')
+	}
+}
+</script>
 
 <template>
 	<div>pwa-notification</div>
+	<div>
+		<button @click="requestNotificationPermission">Enable Notification</button>
+		<div>{{ requestResult }}</div>
+	</div>
 </template>
 
 <style scoped></style>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		VitePWA({
+			strategies: 'injectManifest', // the vite-plugin-pwa plugin will compile your custom service worker and inject its precache manifest
+			injectRegister: 'inline', // register the service worker myself
+			srcDir: 'src/service-worker',
+			filename: 'sw.ts',
+			devOptions: { enabled: false },
+			injectManifest: {
+				injectionPoint: undefined, // prevent from Error: Unable to find a place to inject the manifest
+			},
+			manifest: {
+				name: 'PWA Notification',
+				short_name: 'PWA Notification', // 會顯示在 web app 的名稱
+				theme_color: '#fff',
+				description: 'this is description',
+				icons: [
+					{
+						src: '/logo.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+				],
+			},
+		}),
+	],
 })
