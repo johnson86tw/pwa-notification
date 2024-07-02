f236e58146c7116180c9eaf80f54200b27ca967d
Johnson Chen <chnejohnson@gmail.com>
2024-07-02 15:36:40 +0800

    update sw.ts

--- frontend/src/service-worker/sw.ts
+++ frontend/src/service-worker/sw.ts
@@ -1,1 +1,12 @@
-console.log('this message is from the service worker 1')
+/// <reference lib="webworker" />
+
+console.log('New service worker is installing...')
+
+declare const self: ServiceWorkerGlobalScope
+
+// To force the new service worker to activate immediately
+self.addEventListener('install', () => void self.skipWaiting())
+
+self.addEventListener('activate', () => {
+	self.registration.showNotification('Service worker is activated')
+})
